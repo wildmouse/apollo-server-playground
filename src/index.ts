2835ee424c25c126172180beda1ca84589a79777
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-04-19 21:27:49 +0900

    Add simple mutation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import * as express from 'express'
-import { ApolloServer, gql } from 'apollo-server-express'
+import {ApolloServer, gql} from 'apollo-server-express'
 
 const typeDefs = gql`
-    type Query { 
-        hello: String
+    type Query {
+        hello: String!
+    }
+
+    type Mutation {
+        say(name: String!): String!
     }
 `
 
@@ -11,13 +15,20 @@ const resolvers = {
     Query: {
         hello: () => "Hello, world!",
     },
+    Mutation: {
+        say: (parent, args) => {
+            const greeting = `Hello, ${args.name}!`
+            // Do something with greeting
+            return true
+        }
+    }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({typeDefs, resolvers})
 
 const app = express()
-server.applyMiddleware({ app })
+server.applyMiddleware({app})
 
-app.listen({ port: 4000 }, () => {
+app.listen({port: 4000}, () => {
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 })
